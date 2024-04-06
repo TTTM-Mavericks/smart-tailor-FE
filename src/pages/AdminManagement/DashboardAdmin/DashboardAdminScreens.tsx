800801f0a96b877a1e9c37256296e31a1fe6b79f
Mai Thanh Tâm <maithanhtam13122002@gmail.com>
2024-04-06 20:42:25 +0700

    [FE0023] Create Geography Chart And Fix Bugs Admin Dashboard

--- src/pages/AdminManagement/DashboardAdmin/DashboardAdminScreens.tsx
+++ src/pages/AdminManagement/DashboardAdmin/DashboardAdminScreens.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import TopbarComponent from '../GlobalComponent/TopBar/TopBarComponent';
 import SideBarComponent from '../GlobalComponent/SideBar/SideBarComponent';
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline, useMediaQuery, useTheme } from "@mui/material";
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import theme from '../../../theme';
 import "./DashboardAdminStyle.css"
@@ -9,15 +9,20 @@ import ManageUsers from '../ManageUsers/ManageUsersScreens';
 import LineChartComponent from '../LineChart/LineChartComponent';
 import BarChartComponent from '../BarChart/BarChartComponent';
 import PieChartComponent from '../PieChart/PieChartComponent';
+import GeographyChartComponent from '../GeographyChart/GeographyChartComponent';
 
 export default function DashboardAdminScreens() {
+    const theme1 = useTheme();
+    const smScreen = useMediaQuery(theme1.breakpoints.up("sm"));
     return (
+
         <CssVarsProvider theme={theme}>
             <CssBaseline />
             <div className="dashboard">
                 <SideBarComponent />
                 <main className="content">
                     <TopbarComponent />
+                    <GeographyChartComponent />
                     <LineChartComponent />
                     <BarChartComponent />
                     <PieChartComponent />
---
+++ src/pages/AdminManagement/GeographyChart/GeographyChartComponent.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import GeographyChart from "./GeographyChartScreens";
+
+const GeographyChartComponent = () => {
+    return (
+        <Box m="20px">
+            <Box height="75vh">
+                <GeographyChart />
+            </Box>
+        </Box>
+    );
+};
+
+export default GeographyChartComponent;
