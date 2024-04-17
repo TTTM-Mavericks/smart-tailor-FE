08ee09e75429dedbd9d24ebf6e6bb1d2d238d921
DoopTu <tuhandsome111@gmail.com>
2024-04-17 22:48:07 +0700

    [FE0014] Create Footer component

--- src/pages/Home/HomeScreen.tsx
+++ src/pages/Home/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HeaderComponent from '../../components/Header/HeaderComponent';
+import FooterComponent from '../../components/Footer/FooterComponent';
 
 const HomeScreen = () => {
     return (
         <div>
             <HeaderComponent></HeaderComponent>
+            <FooterComponent></FooterComponent>
         </div>
     );
 };
