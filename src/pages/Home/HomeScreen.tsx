6a90ffd367a3399d024c449682989144e30bb87a
DoopTu <tuhandsome111@gmail.com>
2024-04-17 21:59:57 +0700

    [FE0014] Config tailwind and post css

---
+++ src/pages/Home/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import HeaderComponent from '../../components/Header/HeaderComponent';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <HeaderComponent></HeaderComponent>
+        </div>
+    );
+};
+
+
+
+export default HomeScreen;
