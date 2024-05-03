9e2cd1e687a60e8e953329018ce459f5c441b863
DoopTu <tuhandsome111@gmail.com>
2024-05-03 20:08:50 +0700

    [FE0005-0006] Update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,45 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// import fs from 'fs/promises';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     loader: 'jsx',
+//   },
+//   resolve: {
+//     alias: {
+//       './runtimeConfig': './runtimeConfig.browser',
+//     },
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       loader: {
+//         '.js': 'jsx',
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-import fs from 'fs/promises';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
   },
   resolve: {
     alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
