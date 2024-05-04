2387c02c021c6ab1fe7449eb8e5add5c3f3ef1fb
DoopTu <tuhandsome111@gmail.com>
2024-05-04 13:05:18 +0700

    [FE0041] Update mutil language for Sign in screen

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,21 @@ import ai from "./ai.png";
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
-
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
-
 import systemLogo from "./system/smart-tailor_logo.png"
-
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt, systemLogo };
+import vietnamFlag from './system/vietnam_flag.png'
+import usaFlag from './system/usa_flag.png'
+import languageIcon from './system/language.png'
+export {
+    ai,
+    fileIcon,
+    swatch,
+    download,
+    logoShirt,
+    stylishShirt,
+    systemLogo,
+    vietnamFlag,
+    usaFlag,
+    languageIcon
+};
