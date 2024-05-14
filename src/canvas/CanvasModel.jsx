a95efb3187d4380e7a18382b0cc81383abb94be6
DoopTu <tuhandsome111@gmail.com>
2024-05-14 17:42:59 +0700

    [FE0040] Create 3D model base on glb file

--- src/canvas/CanvasModel.jsx
+++ src/canvas/CanvasModel.jsx
@@ -3,13 +3,19 @@ import { Environment, PresentationControls } from '@react-three/drei';
 
 import Shirt from './Shirt';
 import styles from './Canva.module.scss';
+import Hoodie from './Hoodie';
+import { PaddingTwoTone } from '@mui/icons-material';
+import Pant from './Pant';
+import LongShirt from './LongShirt';
 
 const CanvasModel = () => {
 
     return (
-        <Canvas
+        <>
+
+            {/* <Canvas
             shadows
-            camera={{ position: [0, 0, 5], fov: 65}}
+            camera={{ position: [0, 0, 5], fov: 65 }}
             gl={{ preserveDrawingBuffer: true }}
             className={styles.canvaContainer}
         >
@@ -18,7 +24,49 @@ const CanvasModel = () => {
             <PresentationControls speed={1.5} global zoom={0.7} polar={[-1, Math.PI / 2]}>
                 <Shirt />
             </PresentationControls>
-        </Canvas>
+        </Canvas> */}
+
+            {/* <Canvas
+                shadows
+                camera={{ position: [0, 0, 700], fov: 10 }}
+                gl={{ preserveDrawingBuffer: true }}
+                className={styles.canvaContainer__hoodie}
+            >
+                <ambientLight intensity={0.5} />
+                <Environment preset="city" />
+                <PresentationControls speed={1.5} global zoom={0.7} polar={[-1, Math.PI / 2]} >
+                    <Hoodie />
+                </PresentationControls>
+            </Canvas> */}
+
+            {/* <Canvas
+                shadows
+                camera={{ position: [0.5, -10, 5], fov: 35 }}
+                gl={{ preserveDrawingBuffer: true }}
+                className={styles.canvaContainer__pant}
+            >
+                <ambientLight intensity={0.5} />
+                <Environment preset="city" />
+                <PresentationControls speed={1.5} global zoom={0.7} polar={[-1, Math.PI / 2]} >
+                    <Pant />
+                </PresentationControls>
+            </Canvas> */}
+
+            <Canvas
+                shadows
+                camera={{ position: [0, 0, 50], fov: 65 }}
+                gl={{ preserveDrawingBuffer: true }}
+                className={styles.canvaContainer__longShirt}
+            >
+                <ambientLight intensity={0.5} />
+                <Environment preset="city" />
+                <PresentationControls speed={1.5} global zoom={0.7} polar={[-1, Math.PI / 2]} >
+                    <LongShirt />
+                </PresentationControls>
+            </Canvas>
+
+
+        </>
     );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from 'valtio';
 
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: '#FFFFFF',
   
   isLogoTexture: false,
   logoDecal: './threejs.png',
