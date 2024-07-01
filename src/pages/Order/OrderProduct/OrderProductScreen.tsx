0e30786cf856cf48cdd19cc96e9d4b1f0b60a62a
DoopTu <tuhandsome111@gmail.com>
2024-07-01 23:20:08 +0700

    [FE0044] Update input size and quantity

--- src/pages/Order/OrderProduct/OrderProductScreen.tsx
+++ src/pages/Order/OrderProduct/OrderProductScreen.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import style from './OrderProductStyle.module.scss'
 import HeaderComponent from '../../../components/Header/HeaderComponent';
 import FooterComponent from '../../../components/Footer/FooterComponent';
 import ChangeAddressDialogComponent from './ChangeAddressDialogComponent';
-import { TextField, IconButton, Button, Box, Grid, Container, Autocomplete } from '@mui/material';
-import { Add as AddIcon, Remove as RemoveIcon } from '@mui/icons-material';
+import { TextField, Grid, Autocomplete } from '@mui/material';
 import { styled } from '@mui/system';
 import { primaryColor } from '../../../root/ColorSystem';
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
 
 
 interface SizeQuantity {
@@ -116,64 +117,71 @@ const OrderProductScreen = () => {
     };
 
     return (
-        <div className={`${style.orderProduct_container}`}>
+        <div className={`${style.orderProduct__container}`}>
             <HeaderComponent></HeaderComponent>
             <div>
                 <div className="py-0 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
                     <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
                         <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
                             <p className="text-md md:text-xl light:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Order details</p>
-                            <div className="flex justify-start items-start light:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
+                            <div className="flex justify-start items-start light:bg-gray-800 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
                                 <div className="pb-4 mr-10 md:pb-8 w-full md:w-40">
                                     <img className="w-full hidden md:block" src="https://i.ibb.co/84qQR4p/Rectangle-10.png" alt="dress" />
                                     <img className="w-full md:hidden" src="https://i.ibb.co/L039qbN/Rectangle-10.png" alt="dress" />
                                 </div>
                                 <div className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-                                    <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
-                                        <div className="w-full flex flex-col justify-start items-start space-y-8" >
-                                            <h3 className="text-md light:text-white xl:text-1xl font-semibold leading-6 text-gray-800">Premium Quaility Dress</h3>
-                                            <div className="flex justify-start items-start flex-col space-y-2">
-                                                <div>
-                                                    {sizeQuantities.map((sq, index) => (
-                                                        <Grid container spacing={5} alignItems="center" key={index}>
-                                                            <Grid item>
-                                                                <Autocomplete
-                                                                    options={sizes}
-                                                                    value={sq.size}
-                                                                    onChange={(event, newValue) => newValue && __handleSizeChange(index, newValue)}
-                                                                    renderInput={(params) => <CustomTextField {...params} label="Size" variant="outlined" />}
-                                                                />
+                                    <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0" >
+                                        <div className="w-full justify-start items-start space-y-8" >
+                                            <div className="flex justify-between space-x-8 items-start w-full">
+                                                <h3 className="text-md light:text-white xl:text-1xl font-semibold leading-6 text-gray-800">Premium Quaility Dress</h3>
+                                                <p className="text-base light:text-white xl:text-lg leading-6">$36.00 <span className="text-red-300 line-through"> $45.00</span></p>
+                                                <p className="text-base light:text-white xl:text-lg leading-6 text-gray-800">01</p>
+                                                <p className="text-base light:text-white xl:text-lg font-semibold leading-6 text-gray-800">$36.00</p>
+                                            </div>
+                                            <div className={`${style.orderProduct__container__content}`}>
+                                                <div className={`${style.orderProduct__container__information}`}>
+                                                    <p className="text-sm light:text-white leading-none text-gray-800"><span className="light:text-gray-400 text-gray-300">Style: </span> Italic Minimal Design</p>
+                                                    <p className="text-sm light:text-white leading-none text-gray-800"><span className="light:text-gray-400 text-gray-300">Size: </span> Small</p>
+                                                    <p className="text-sm light:text-white leading-none text-gray-800"><span className="light:text-gray-400 text-gray-300">Color: </span> Light Blue</p>
+                                                </div>
+                                                <div className={`${style.orderProduct__container__detail}`}>
+                                                    <div className={`${style.orderProduct__container__detail__sizeDetail}`}>
+                                                        {sizeQuantities.map((sq, index) => (
+                                                            <Grid container spacing={2} alignItems="center" key={index} className='mb-3'>
+                                                                <Grid item>
+                                                                    <Autocomplete
+                                                                        options={sizes}
+                                                                        value={sq.size}
+                                                                        onChange={(event, newValue) => newValue && __handleSizeChange(index, newValue)}
+                                                                        renderInput={(params) => <CustomTextField {...params} label="Size" variant="outlined" />}
+                                                                    />
+                                                                </Grid>
+                                                                <Grid className={`${style.orderProduct__container__detail__sizeDetail__quantity}`} item>
+                                                                    <CustomTextFieldQuantity
+                                                                        fullWidth
+                                                                        type="number"
+                                                                        label="Quantity"
+                                                                        value={sq.quantity}
+                                                                        onChange={(e) => __handleQuantityChange(index, Number(e.target.value))}
+                                                                        inputProps={{ min: 1 }}
+                                                                        style={{ marginRight: 10 }}
+
+                                                                    />
+                                                                    <FaMinusCircle size={25} color={primaryColor} onClick={() => __handleRemoveSizeQuantity(index)} style={{ display: sizeQuantities.length === 1 ? 'none' : 'flex', cursor: 'pointer' }}>
+                                                                    </FaMinusCircle>
+                                                                </Grid>
                                                             </Grid>
-                                                            <Grid item>
-                                                                <CustomTextFieldQuantity
-                                                                    fullWidth
-                                                                    type="number"
-                                                                    label="Quantity"
-                                                                    value={sq.quantity}
-                                                                    onChange={(e) => __handleQuantityChange(index, Number(e.target.value))}
-                                                                    inputProps={{ min: 1 }}
-                                                                />
-                                                            </Grid>
-                                                            <Grid item xs={2}>
-                                                                <IconButton onClick={() => __handleRemoveSizeQuantity(index)} disabled={sizeQuantities.length === 1}>
-                                                                    <RemoveIcon />
-                                                                </IconButton>
-                                                            </Grid>
-                                                        </Grid>
-                                                    ))}
-                                                    <div>
-                                                        <IconButton onClick={__handleAddSizeQuantity}>
-                                                            <AddIcon />
-                                                        </IconButton>
+                                                        ))}
+                                                        <div>
+                                                            <FaPlusCircle size={20} style={{ cursor: 'pointer' }} color={primaryColor} onClick={__handleAddSizeQuantity}>
+                                                            </FaPlusCircle>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
+
                                         </div>
-                                        <div className="flex justify-between space-x-8 items-start w-full">
-                                            <p className="text-base light:text-white xl:text-lg leading-6">$36.00 <span className="text-red-300 line-through"> $45.00</span></p>
-                                            <p className="text-base light:text-white xl:text-lg leading-6 text-gray-800">01</p>
-                                            <p className="text-base light:text-white xl:text-lg font-semibold leading-6 text-gray-800">$36.00</p>
-                                        </div>
+
                                     </div>
                                 </div>
 
