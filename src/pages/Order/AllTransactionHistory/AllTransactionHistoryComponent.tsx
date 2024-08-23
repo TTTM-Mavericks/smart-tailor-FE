350b6c2ca45404ff0ef4b63ba6e1768a4ba6e993
DoopTu <tuhandsome111@gmail.com>
2024-08-23 15:04:09 +0700

    [FE0044] Update interface for order processing

--- src/pages/Order/AllTransactionHistory/AllTransactionHistoryComponent.tsx
+++ src/pages/Order/AllTransactionHistory/AllTransactionHistoryComponent.tsx
@@ -460,7 +460,7 @@ const AllTransactionHistoryComponent: React.FC = () => {
                                 Refund Transaction
                             </a>
                             <a href="/transaction_history" className="px-4 py-3 font-semibold text-gray-700 border border-gray-200 rounded-lg hover:bg-gray-100">
-                                Trandsactions
+                                Transactions
                             </a>
                             <a href="/collection" className="px-4 py-3 font-semibold text-gray-700 border border-gray-200 rounded-lg hover:bg-gray-100">
                                 Collection
